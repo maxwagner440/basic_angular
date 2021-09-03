85da42ae070a9f5792b4fe109e00f8f6de364ae9
maxwagner440 <maxwagner440@gmail.com>
2021-09-03 10:25:52 -0400

    adding template from malec

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WebsiteModule } from './website/website.module';
+import { NavModule } from './nav/nav.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    WebsiteModule,
+    NavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
